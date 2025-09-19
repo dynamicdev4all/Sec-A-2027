528e1858684ae1d00514b8e45537b15d700bfa8f
dynamicdev4all <thepurpleaxe@duck.com>
2025-09-19 15:57:31 +0530

    array list demo code

---
+++ ArrayListDemo.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        int arr[] = new int[5];//this is Array init
+        ArrayList<String> list = new ArrayList<>(); // this is ArrayList init
+
+
+        //Data Insertion
+        list.add("SampleData");//this is for ArrayList
+        arr[0] = 123;//this is for Array
+
+        //Data Fetch
+        list2.get(6); // this is used to fetch data from 6th index
+        arr[6];// this is the syntax for Array, this is also fetching data from 6th Index
+
+
+        //to check if the list/Array is empty or not
+        list2.isEmpty(); // this returns true if the list is empty and false if the list has some data
+        arr.length == 0; // this also does the same thing as in ArrayList
+
+
+        //to check the size of an Array or ArrayList
+
+        list.size(); //this returns the actual size of the ArrayList
+        arr.length;//this does the same thing as above.
+ 
+    }
+}
--- PrintDigit.java
+++ PrintDigit.java
@@ -7,6 +7,13 @@ public class PrintDigit {
         System.out.println(n);
         printRevDigits(n - 1);
     }
+    static void printDigits(int n) {
+        if (n <= 0) {
+            return;
+        }
+        printDigits(n - 1);
+        System.out.println(n);
+    }
 
     public static void main(String[] args) {
 
